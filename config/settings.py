ef83d01182f19c8ef5ec0476f31736203317f4dc
Jiyun Woo <129712594+jiyunwoo02@users.noreply.github.com>
2024-06-11 15:36:36 +0900

    0611 회원가입 테스트 완료

--- config/settings.py
+++ config/settings.py
@@ -54,6 +54,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+# 이거 필수 - 이거로 회원가입 API 성공 0611
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
