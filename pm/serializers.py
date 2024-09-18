b266dfee96be78f7f8911a4ac1eff77950f73fec
Jiyun Woo <jiyunwoo33@gmail.com>
2024-09-18 17:10:20 +0900

    0918 서버에 제품정보 제공 구현

--- pm/serializers.py
+++ pm/serializers.py
@@ -1,10 +1,17 @@
-from rest_framework import serializers
-from .models import User
-
-# 회원가입 API 생성에 필요
 # Serializer: 데이터 직렬화
 # -- python 객체나 django에서 queryset 등 복잡한 객체들을 REST API에서 사용 가능하게 json 형태로 변환
+
+from rest_framework import serializers
+from .models import User, Goods
+
+# 회원가입 API
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['login_id', 'username', 'userpwd']
+
+# 제품 정보 제공 API: 제품 데이터를 JSON 형식으로 직렬화
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = ['goods_id', 'goodsname', 'category', 'brand', 'goodsdesc', 'goodsimg', 'price', 'discountprice']
--- pm/urls.py
+++ pm/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import signup_api
 from .views import login_api
+from .views import GoodsList, GoodsDetail
 
 
 urlpatterns = [
     path('signup/', signup_api, name='signup'), # 회원가입
     path('login/', login_api, name='login'), # 로그인
+    path('goods/', GoodsList.as_view(), name='goods-list'),  # 전체 제품 목록
+    path('goods/<int:goods_id>/', GoodsDetail.as_view(), name='goods-detail'),  # 개별 제품 상세
 ]
